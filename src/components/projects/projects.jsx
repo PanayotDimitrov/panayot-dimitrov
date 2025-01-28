378bc020d2cc915e2dc0ecad5cb768b4ae87b2b2
Panayot Dimitrov <paceto2014@gmail.com>
2025-01-28 13:56:16 +0200

    changes

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./projects.css";
 import project from "../../../pictures/project.jpg";
+import project1 from "../../../pictures/c.jpg";
+import project2 from "../../../pictures/management-system.png";
+import project3 from "../../../pictures/drawing.jpg";
+import project4 from "../../../pictures/carshop.webp";
 
 const projects = () => {
   return (
@@ -9,25 +13,37 @@ const projects = () => {
       <div className="container">
         <ul className="projects-list">
           <li className="project">
-            <img src={project} alt="Project" height={200} />
-            <h3>Service management system</h3>
-            <p>
-              description //todo
-            </p>
+            <img src={project1} alt="Project" height={200} />
+            <h3>Simple-C Project</h3>
+            <div className="project-links">
+              <a href="https://github.com/PanayotDimitrov/SimpleC-project">Code</a>
+            </div>
           </li>
           <li className="project">
-            <img src={project} alt="Project" height={200} />
-            <h3>Service management system</h3>
-            <p>
-              description //todo
-            </p>
+            <img src={project2} alt="Project" height={200} />
+            <h3>Service Management System Project</h3>
+            <div className="project-links">
+              <a href="https://github.com/PanayotDimitrov/service-management-system">
+                Backend
+              </a>
+              <a href="https://github.com/PanayotDimitrov/service-management-system-frontend">
+                Frontend
+              </a>
+            </div>
           </li>
           <li className="project">
-            <img src={project} alt="Project" height={200} />
-            <h3>Service management system</h3>
-            <p>
-              description //todo
-            </p>
+            <img src={project3} alt="Project" height={200} />
+            <h3>Drawing project</h3>
+            <div className="project-links">
+              <a href="https://github.com/PanayotDimitrov/Drawing-project">Code</a>
+            </div>
+          </li>
+          <li className="project">
+            <img src={project4} alt="Project" height={200} />
+            <h3>Car Shop Project</h3>
+            <div className="project-links">
+              <a href="https://github.com/PanayotDimitrov/Car-Shop">Code</a>
+            </div>
           </li>
         </ul>
       </div>
